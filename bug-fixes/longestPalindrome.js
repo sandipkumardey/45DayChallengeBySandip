c23bbbca026cb9047a11fda3309e877a3ea16354
Anshu-bhatt <148609797+Anshu-bhatt@users.noreply.github.com>
2024-09-18 18:19:26 +0530

    Update longestPalindrome.js
    fixed the bug for full string palindromic

--- bug-fixes/longestPalindrome.js
+++ bug-fixes/longestPalindrome.js
@@ -1,19 +1,29 @@
-// Longest Palindromic Substring - Fails with full-string palindromes
 function longestPalindrome(s) {
-    let maxLen = 0;
-    let start = 0;
+  let maxLen = 0;
+  let start = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i; j < s.length; j++) {
-            let substring = s.substring(i, j + 1);
-            let reversed = substring.split('').reverse().join('');
-
-            if (substring === reversed && substring.length > maxLen) {
-                maxLen = substring.length;
-                start = i;
-            }
-        }
+  for (let i = 0; i < s.length; i++) {
+    let left = i - 1;
+    let right = i + 1;
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > maxLen) {
+        maxLen = right - left + 1;
+        start = left;
+      }
+      left--;
+      right++;
     }
+    left = i;
+    right = i + 1;
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > maxLen) {
+        maxLen = right - left + 1;
+        start = left;
+      }
+      left--;
+      right++;
+    }
+  }
 
-    return s.substring(start, start + maxLen); // Fails for palindromes that span the entire string
+  return s.substring(start, start + maxLen);
 }
